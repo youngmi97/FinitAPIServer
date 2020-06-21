c5ee3daa79cbe784676b4af0417f6da710321c00
youngmi97 <jeong7331@kaist.ac.kr>
2020-06-22 03:40:36 +0900

    graphql interface localhost

--- schema.js
+++ schema.js
@@ -1,12 +1,46 @@
-const GraphQLObjectType = require('graphql');
-
+// Bring in all types that are used
+const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLSchema } = require('graphql');
+const axios = require('axios');
 
 // Alot of different types with graphql
 // e.g. Object Type 
+// Better to create separate object with nested json object data
 
+
+// Test with SpaceX Api
 const LaunchType = new GraphQLObjectType({
     name: 'Launch',
     fields: () => ({
-        
+        flight_number: { type: GraphQLInt },
+        mission_name: { type: GraphQLString },
+        rocket: { type: RocketType }
     })
+});
+
+
+const RocketType = new GraphQLObjectType({
+    name: 'Rocket',
+    fields: () => ({
+        rocket_id: { type: GraphQLString }
+    })
+});
+
+
+//Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        launches: {
+            type: new GraphQLList(LaunchType),
+            resolve(parent, args) {
+                return axios.get(
+                  "https://api.spacexdata.com/v3/launches")
+                  .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const graphqlHTTP = require('express-graphql');
 const schema = require('./schema.js');
 const plaid = require("plaid");
 
@@ -7,22 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-const plaidClient = new plaid.Client(
-  process.env.PLAID_CLIENT_ID,
-  process.env.PLAID_SECRET,
-  process.env.PUBLIC_KEY,
-  plaid.environments.sandbox,
-  { version: "2018-05-22" }
-);
-
 //single endpoint
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema,
-        //client to make req to server
-        graphiql: true,
-    }),
-);
+app.use('/graphql', graphqlHTTP({
+    schema,
+    //client to make req to server
+    graphiql: true
+}));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
