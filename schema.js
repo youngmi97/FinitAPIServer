b054b2efb7d42accd6baed1a94a8a12e4e9f6ba6
Jae Hwan Jeong <jeong7331@Jaes-MacBook-Pro-2.local>
2020-06-22 03:13:39 +0900

    initial commit

---
+++ schema.js
@@ -0,0 +1,12 @@
+const GraphQLObjectType = require('graphql');
+
+
+// Alot of different types with graphql
+// e.g. Object Type 
+
+const LaunchType = new GraphQLObjectType({
+    name: 'Launch',
+    fields: () => ({
+        
+    })
+});
