7bea8bf68fe7b00ebdd68cdf5a00b934a74a30bd
youngmi97 <jeong7331@kaist.ac.kr>
2020-08-06 16:43:37 +0900

    test context persistence and post login navbar render:

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { AuthContext } from '../context/auth';
 
 function MenuBar() {
+
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
 
   // configure path name parsing logic
@@ -12,7 +15,24 @@ function MenuBar() {
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   //identify login state 
-  const menuBar = (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name={user.username}
+        active
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="logout"
+          onClick={logout}
+        />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
@@ -39,7 +59,7 @@ function MenuBar() {
         />
       </Menu.Menu>
     </Menu>
-  )
+  );
 
   return menuBar;
 }
