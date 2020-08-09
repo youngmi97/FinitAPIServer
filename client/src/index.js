42e18de85227ddc6839d88cef853e19f3c0ae837
Jinjae Kim <jinj.kim97@gmail.com>
2020-08-09 17:47:29 +0900

    first design commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from './App';
+
 import * as serviceWorker from "./serviceWorker";
 import ApolloProvider from "./ApolloProvider";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.render(ApolloProvider, document.getElementById("root"));
 
