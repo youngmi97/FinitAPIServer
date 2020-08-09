1d0ae7c27a7a4168b798527734c40cb9ac8e6c53
Jinjae Kim <jinj.kim97@gmail.com>
2020-08-09 17:59:27 +0900

    added alias #components

--- client/src/components/Card_internal.js
+++ client/src/components/Card_internal.js
@@ -21,163 +21,163 @@ import Avatar from "@material-ui/core/Avatar";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		minWidth: 217,
-		minHeight: 217,
-		maxwidth: 345,
-	},
-	title: {
-		fontSize: 14,
-		fontWeight: 500,
-	},
-	pos: {
-		marginBottom: 12,
-	},
-	media: {
-		height: 186,
-	},
-	overlay: {
-		position: "absolute",
-		top: "0%",
-		left: "75%",
-		color: "white",
-	},
-	ListItemSize: {
-		marginLeft: 10,
-		"& span, & svg": {
-			fontSize: "14px",
-			fontWeight: 600,
-		},
-	},
-	ListItemSize2: {
-		marginLeft: 20,
-		"& span, & svg": {
-			fontSize: "0.7rem",
-		},
-	},
-	ListItemSize3: {
-		fontSize: "14px",
-	},
-	ListItemSize4: {
-		marginLeft: 20,
-		"& span, & svg": {
-			fontSize: "12px",
-			color: "gray",
-		},
-	},
-	backdrop: {
-		zIndex: theme.zIndex.drawer + 1,
-		color: "#fff",
-	},
-	Buttoncolor: {
-		color: theme.palette.primary.main,
-	},
+  root: {
+    minWidth: 217,
+    minHeight: 217,
+    maxwidth: 345,
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  media: {
+    height: 186,
+  },
+  overlay: {
+    position: "absolute",
+    top: "0%",
+    left: "75%",
+    color: "white",
+  },
+  ListItemSize: {
+    marginLeft: 10,
+    "& span, & svg": {
+      fontSize: "14px",
+      fontWeight: 600,
+    },
+  },
+  ListItemSize2: {
+    marginLeft: 20,
+    "& span, & svg": {
+      fontSize: "0.7rem",
+    },
+  },
+  ListItemSize3: {
+    fontSize: "14px",
+  },
+  ListItemSize4: {
+    marginLeft: 20,
+    "& span, & svg": {
+      fontSize: "12px",
+      color: "gray",
+    },
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+  Buttoncolor: {
+    color: theme.palette.primary.main,
+  },
 }));
 
 export default function Card_internal() {
-	const classes = useStyles();
-	const [open, setOpen] = React.useState(false);
-	const handleClose = () => {
-		setOpen(false);
-	};
-	const handleToggle = () => {
-		setOpen(!open);
-	};
-	return (
-		<Paper>
-			<Card className={classes.root}>
-				<CardHeader
-					avatar={
-						<Avatar
-							aria-label="subscription"
-							variant="rounded"
-							className={classes.avatar}
-							src="static/images/Netflix.png"
-						>
-							N
-						</Avatar>
-					}
-					title={<Typography className={classes.title}>Notion</Typography>}
-					subheader="Team Plan"
-				/>
-				<CardContent>
-					<Typography className={classes.pos} variant="body2" component="p">
-						US $8.00/mo
-						<br />
-						Next payment is in 3 days
-					</Typography>
-					<Typography
-						className={classes.pos}
-						variant="caption"
-						color="textSecondary"
-					>
-						Member since: July 30,2018
-					</Typography>
-				</CardContent>
-				<CardActionArea>
-					<div align="center">
-						<Button
-							size="small"
-							variant="outlined"
-							className={classes.ListItemSize3}
-							onClick={handleToggle}
-						>
-							View Details
-						</Button>
-						<Backdrop
-							className={classes.backdrop}
-							open={open}
-							onClick={handleClose}
-						>
-							<Paper>
-								<List
-									component="nav"
-									aria-labelledby="nested-list-subheader"
-									className={classes.list}
-								>
-									<ListItem button>
-										<Grid
-											container
-											spacing={0}
-											align="center"
-											justify="center"
-											direction="row"
-										>
-											<Grid item xs>
-												<Avatar
-													variant="square"
-													className={classes.small}
-													src="Netflix_small.png"
-												/>
-											</Grid>
-											<Grid item xs>
-												<ListItemText
-													className={classes.ListItemSize}
-													primary="Netflix"
-												/>
-											</Grid>
-											<Grid item xs></Grid>
-										</Grid>
-									</ListItem>
-									<ListItemText
-										className={classes.ListItemSize2}
-										primary="TeamPlan"
-									/>
-									<ListItemText
-										className={classes.ListItemSize2}
-										primary="USD $12.00"
-									/>
-									<br />
-									<ListItemText
-										className={classes.ListItemSize4}
-										primary="Monthly Payment"
-									/>
-								</List>
-							</Paper>
-						</Backdrop>
-					</div>
-				</CardActionArea>
-			</Card>
-		</Paper>
-	);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+  return (
+    <Paper>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar
+              aria-label="subscription"
+              variant="rounded"
+              className={classes.avatar}
+              src="assets/images/Netflix.png"
+            >
+              N
+            </Avatar>
+          }
+          title={<Typography className={classes.title}>Notion</Typography>}
+          subheader="Team Plan"
+        />
+        <CardContent>
+          <Typography className={classes.pos} variant="body2" component="p">
+            US $8.00/mo
+            <br />
+            Next payment is in 3 days
+          </Typography>
+          <Typography
+            className={classes.pos}
+            variant="caption"
+            color="textSecondary"
+          >
+            Member since: July 30,2018
+          </Typography>
+        </CardContent>
+        <CardActionArea>
+          <div align="center">
+            <Button
+              size="small"
+              variant="outlined"
+              className={classes.ListItemSize3}
+              onClick={handleToggle}
+            >
+              View Details
+            </Button>
+            <Backdrop
+              className={classes.backdrop}
+              open={open}
+              onClick={handleClose}
+            >
+              <Paper>
+                <List
+                  component="nav"
+                  aria-labelledby="nested-list-subheader"
+                  className={classes.list}
+                >
+                  <ListItem button>
+                    <Grid
+                      container
+                      spacing={0}
+                      align="center"
+                      justify="center"
+                      direction="row"
+                    >
+                      <Grid item xs>
+                        <Avatar
+                          variant="square"
+                          className={classes.small}
+                          src="Netflix_small.png"
+                        />
+                      </Grid>
+                      <Grid item xs>
+                        <ListItemText
+                          className={classes.ListItemSize}
+                          primary="Netflix"
+                        />
+                      </Grid>
+                      <Grid item xs></Grid>
+                    </Grid>
+                  </ListItem>
+                  <ListItemText
+                    className={classes.ListItemSize2}
+                    primary="TeamPlan"
+                  />
+                  <ListItemText
+                    className={classes.ListItemSize2}
+                    primary="USD $12.00"
+                  />
+                  <br />
+                  <ListItemText
+                    className={classes.ListItemSize4}
+                    primary="Monthly Payment"
+                  />
+                </List>
+              </Paper>
+            </Backdrop>
+          </div>
+        </CardActionArea>
+      </Card>
+    </Paper>
+  );
 }
---
+++ client/src/components/cards/SubscriptionCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Card, CardContent } from "material-ui/core";
+import { classList } from "utils";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 217,
+  },
+}));
+const SubscriptionCard = { title, subtitle, icon, context };
