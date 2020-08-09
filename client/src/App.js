2ac4c74ad5091c247ef29d92a4f3cf84edb0b2b0
Jinjae Kim <jinj.kim97@gmail.com>
2020-08-09 21:39:32 +0900

    added new node-module called styled components

--- client/src/App.js
+++ client/src/App.js
@@ -17,83 +17,83 @@ import MenuBar from "./components/MenuBar";
 import { AuthProvider } from "./context/auth";
 
 const theme = createMuiTheme({
-	palette: {
-		primary: {
-			light: "#9E58EE",
-			main: "#7610EB",
-			dark: "#2F0459",
-			contrastText: "#FFFFFF",
-		},
-		secondary: {
-			light: "#9E58EE",
-			main: "#7610EB",
-			dark: "#2F0459",
-			contrastText: "#FFFFFF",
-		},
-	},
-	background: {
-		default: "#FAFAFA",
-		two: "#F9F9F9",
-		three: "#F7F7F7",
-		four: "#E9E9EA",
-	},
-	typography: {
-		fontFamily: [
-			"-apple-system",
-			"BlinkMacSystemFont",
-			'"Segoe UI"',
-			"Roboto",
-			'"Helvetica Neue"',
-			"Arial",
-			"sans-serif",
-			'"Apple Color Emoji"',
-			'"Segoe UI Emoji"',
-			'"Segoe UI Symbol"',
-		].join(","),
-	},
-	breakpoints: {
-		values: {
-			iphone: 375,
-			tablet: 640,
-			laptop: 1024,
-			desktop: 1440,
-		},
-	},
-	overrides: {
-		MuiButton: {
-			root: {
-				fontSize: "17px",
-				borderRadius: 4,
-				padding: "0 16px",
-				// boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-			},
-			outlined: {
-				fontWeight: 600,
-				Color: "#8a8a8f",
-				borderWidth: 2,
-				borderColor: "#8a8a8f",
-				borderRadius: 20,
-				Height: 32,
-			},
-		},
-	},
+  palette: {
+    primary: {
+      light: "#9E58EE",
+      main: "#7610EB",
+      dark: "#2F0459",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      light: "#9E58EE",
+      main: "#7610EB",
+      dark: "#2F0459",
+      contrastText: "#FFFFFF",
+    },
+  },
+  background: {
+    default: "#FAFAFA",
+    two: "#F9F9F9",
+    three: "#F7F7F7",
+    four: "#E9E9EA",
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+  breakpoints: {
+    values: {
+      iphone: 375,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1440,
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        fontSize: "17px",
+        borderRadius: 4,
+        padding: "0 16px",
+        // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      },
+      outlined: {
+        fontWeight: 600,
+        Color: "#8a8a8f",
+        borderWidth: 2,
+        borderColor: "#8a8a8f",
+        borderRadius: 20,
+        Height: 32,
+      },
+    },
+  },
 });
 
 function App() {
-	return (
-		<AuthProvider>
-			<Router>
-				<ThemeProvider theme={theme}>
-					<MenuBar />
-					<Route exact path="/subscriptions" component={Main} />
-					<Route exact path="/discover" component={Main_Insight} />
-					<Route exact path="/" component={Home_Local} />
-					<Route exact path="/login" component={Login_Local} />
-					<Route exact path="/register" component={Register_Local} />
-				</ThemeProvider>
-			</Router>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <MenuBar />
+          <Route exact path="/subscriptions" component={Main} />
+          <Route exact path="/discover" component={Main_Insight} />
+          <Route exact path="/" component={Home_Local} />
+          <Route exact path="/login" component={Login_Local} />
+          <Route exact path="/register" component={Register_Local} />
+        </ThemeProvider>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
