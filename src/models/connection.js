eeb317d76d1abb126fd8379a180bbcb74ef5adcf
Jae Hwan Jeong <38910144+youngmi97@users.noreply.github.com>
2022-06-04 17:35:32 +0900

    Update connection.js

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const URI =
-  "mongodb+srv://finitdev2:Finit7331!@firstcluster.fhzmk.mongodb.net/test?retryWrites=true&w=majority";
+  "";
 
 mongoose.set("useFindAndModify", false);
 const connectDB = async () => {
